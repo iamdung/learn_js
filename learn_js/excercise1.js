edf268259a8db0d96d2dc5570a9c5ce514c0db41
Tran Tien Dung <dungtrantien11@gmail.com>
2021-07-29 10:44:22 +0700

    câu 2

--- learn_js/excercise1.js
+++ learn_js/excercise1.js
@@ -1,8 +1,13 @@
 var x = 5.3;
 var y = 3.5;
-var age = 16;
-var number = 25;
+var a = 16;
+var b = 25;
 var name = "Tran Tien Dung";
 var description = "siêu đẹp zai";
 var troiMua = true;
 var troiNang = false;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
